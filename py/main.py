3f6a16d8aca0bb19e0507930aed3c52a6fd58a7d
Carter Carlson <carlfarterson@gmail.com>
2019-07-06 17:48:05 -0500

    Add macd

--- py/main.py
+++ py/main.py
@@ -6,18 +6,6 @@ import numpy as np
 df = pd.read_csv('prices/BTC.csv')
 df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d %H:%M:%S')
 
-ema_3 = ema(df['close'], 3)
-ma_20 = ma(df['close'], 20)
-
-len(ma_20) - ma_20.count()
-ma_20.isna().sum()
-
-ema_3 = ema_3[20:]
-ma_20 = ma_20[20:]
-
-
-intersections = cross(ema_3, ma_20)
-
 
 def ma(prices, window):
     return prices.rolling(window=window).mean()
@@ -27,41 +15,45 @@ def ema(prices, window):
     return prices.ewm(span=window, adjust=False).mean()
 
 
-def macd(prices):
-    pass
+def macd(prices, fast=12, slow=26):
+    '''
+    macd line = fast_ema - slow_ema
+    signal line = 9ema of macd line
+    histogram = macd line - signal line
+    '''
+    ema_fast = ema(prices, window=fast)
+    ema_slow = ema(prices, window=slow)
+    return ema_fast - ema_slow
 
 
 def rsi(prices):
-    # Based on last two weeks
-    n = 24 * 14
+    n = 14
     deltas = np.diff(prices)
     seed = deltas[:n+1]
     up = seed[seed > 0].sum()/n
     down = -seed[seed < 0].sum()/n
-    rs = up/down
     rsi = np.zeros_like(prices)
-    rsi[:n] = 100. - 100./(1.+rs)
+    rsi[:n] = 100. - 100./(1.+ up/down)
     for i in range(n, len(prices)):
         delta = deltas[i-1]
         if delta > 0:
             up_val = delta
-            down_val = 0.
+            down_val = 0
         else:
-            up_val = 0.
+            up_val = 0
             down_val = -delta
 
         up = (up*(n-1) + up_val)/n
         down = (down*(n-1) + down_val)/n
 
-        rs = up/down
-        rsi[i] = 100. - 100./(1.+rs)
+        rsi[i] = 100. - 100./(1. + up/down)
 
     return rsi
 
 
 def cross(line1, line2):
     nan_count = max(line1.isna().sum(), line2.isna().sum())
-    crosses = [None for i in range(nan_count)]
+    crosses = [False for i in range(nan_count)]
     l1_gt_l2 = line1 > line2
     current_val = l1_gt_l2[nan_count]
     for next_val in l1_gt_l2[nan_count+1:]:
