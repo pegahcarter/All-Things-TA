bc3eddc497c34a25dc0e1b7ebf2b870f20d1f416
Carter Carlson <carlfarterson@gmail.com>
2019-07-09 15:53:07 -0500

    Update prices and notes

--- py/main.py
+++ py/main.py
@@ -3,10 +3,6 @@ import pandas as pd
 import numpy as np
 
 
-df = pd.read_csv('prices/BTC.csv')
-df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d %H:%M:%S')
-
-
 def ma(prices, window):
     return prices.rolling(window=window).mean()
 
@@ -66,23 +62,26 @@ def cross(line1, line2):
 
 def main():
 
-    ema_3 = ema(prices, window=3)
-    ema_40 = ema(prices, window=40)
-    ma_20 = ma(prices, window=20)
-    macd(prices)
-    rsi(prices)
+    df = pd.read_csv('prices/BTC.csv')
+    df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d %H:%M:%S')
+
+    df_ema_3 = ema(df['close'], window=3)
+    df_ema_40 = ema(df['close'], window=40)
+    df_ma_20 = ma(df['close'], window=20)
+    df_macd = macd(df['close'])
+    df_rsi = rsi(df['close'])
 
     # Logic
     intersections = cross(ema_3, ma_20)
-    ema_40_above = ema_40 > ema_3 & ema_40 > ma_20
-    rsi_above = rsi > 50
-    macd_above = macd > 0
+    ema_40_above = df_ema_40 > df_ema_3 & df_ema_40 > df_ma_20
+    rsi_above = df_rsi > 50
+    macd_above = df_macd > 0
 
     for intersection in intersections:
         if intersection:
             if ema_40_above[i] & rsi_above[i] & macd_above[i]:
                 signal = "buy"
-            elif not ema_40_above[i] & not rsi_above[i] & not macd_above[i]:
+            elif not ema_40_above[i] and not rsi_above[i] and not macd_above[i]:
                 signal = "sell"
 
 
--- starbucks.md
+++ starbucks.md
@@ -1,15 +1,16 @@
-RSI
-- Above 50 to long
-- Below 50 to short
-
-MACD
-- Above 0 to long
-- Below 0 to short
 
 Moving averages
 1. 3 EMA
   - When 3 EMA crosses 20 MA, look for entry based on 40 EMA
+
 2. 40 EMA
   - Above to long
   - Below to short
-3. 20 MA
+
+3. RSI
+  - Above 50 to long
+  - Below 50 to short
+
+4. MACD
+  - Above 0 to long
+  - Below 0 to short
