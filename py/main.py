3e0edd53db8947af9fe9c5278b4980d5765b1ecd
Carter Carlson <carlfarterson@gmail.com>
2019-07-09 16:56:59 -0500

    Add signals to each coin CSV

--- py/main.py
+++ py/main.py
@@ -4,31 +4,32 @@ from py.logic import calc_ma, calc_ema, calc_macd, calc_rsi, cross
 
 def main():
 
-    df = pd.read_csv('prices/BTC.csv')
-    df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d %H:%M:%S')
+    for file in os.listdir('prices/'):
+        df = pd.read_csv('prices/' + file)
+        df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d %H:%M:%S')
 
-    ema_3 = calc_ema(df['close'], window=3)
-    ema_40 = calc_ema(df['close'], window=40)
-    ma_20 = calc_ma(df['close'], window=20)
-    macd = calc_macd(df['close'])
-    rsi = calc_rsi(df['close'])
+        ema_3 = calc_ema(df['close'], window=3)
+        ema_40 = calc_ema(df['close'], window=40)
+        ma_20 = calc_ma(df['close'], window=20)
+        macd = calc_macd(df['close'])
+        rsi = calc_rsi(df['close'])
 
-    # Logic
-    intersections = cross(ema_3, ma_20)
-    ema_40_above = (ema_40 > ema_3) & (ema_40 > ma_20)
-    rsi_above = rsi > 50
-    macd_above = macd > 0
+        # Logic
+        intersections = cross(ema_3, ma_20)
+        ema_40_above = (ema_40 > ema_3) & (ema_40 > ma_20)
+        rsi_above = rsi > 50
+        macd_above = macd > 0
 
-    signals = [None for x in range(len(df))]
-    for i, val in enumerate(intersections):
-        if val:
-            if ema_40_above[i] and rsi_above[i] and macd_above[i]:
-                signals[i] = "BUY"
-            elif not ema_40_above[i] and not rsi_above[i] and not macd_above[i]:
-                signals[i] = "SELL"
+        signals = [None for x in range(len(df))]
+        for i, val in enumerate(intersections):
+            if val:
+                if ema_40_above[i] and rsi_above[i] and macd_above[i]:
+                    signals[i] = "BUY"
+                elif not ema_40_above[i] and not rsi_above[i] and not macd_above[i]:
+                    signals[i] = "SELL"
 
-    df['signals'] = signals
-    df.to_csv('prices/BTC.csv', index=False)
+        df['signals'] = signals
+        df.to_csv('prices/' + file, index=False)
 
 
 if __name__ == '__name__':
