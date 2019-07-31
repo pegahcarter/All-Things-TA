bb3cae5002f5f18423b1ff73e3073467ccacca6c
Carter Carlson <carlfarterson@gmail.com>
2019-07-31 13:29:21 -0500

    Update with daily candle logic

--- py/main.py
+++ py/main.py
@@ -7,29 +7,30 @@ from urllib.parse import urlencode
 import requests
 import time
 
-text = ''
-signal_df = []
+for candle_abv, candle_string in candle_intervals.items():
+    text = candle_string + '\n'
+    signal_df = []
 
-for ticker in tickers:
-    data = bitmex.fetch_ohlcv(ticker, '1h', limit=500, since=since)
-    df = pd.DataFrame(data=data, columns=['date', 'open', 'high', 'low', 'close', 'volume'])
-    df['date'] = [datetime.fromtimestamp(x/1000) for x in df['date']]
+    for ticker in tickers:
+        data = binance.fetch_ohlcv(ticker, candle_abv)
+        df = pd.DataFrame(data=data, columns=['date', 'open', 'high', 'low', 'close', 'volume'])
+        df['date'] = [datetime.fromtimestamp(x/1000) for x in df['date']]
 
-    coin_signals = logic.run(ticker, df)
-    signal_df += coin_signals
+        coin_signals = logic.run(ticker, df)
+        signal_df += coin_signals
 
-signal_df = pd.DataFrame(signal_df, columns=['date', 'signal', 'coin', 'price'])
-signal_df = signal_df.sort_values('date').reset_index(drop=True)
-signal_df.to_csv('../signals.csv', index=False)
-# old_signal_df = pd.read_csv('../signals.csv')
-# new_signals = signal_df.loc[signal_df['date'] > old_signal_df['date'].iloc[-1]]
-#
-# if len(new_signals) > 0:
-#     for _, row in new_signals.iterrows():
-#         date = row['date'].strftime('%m/%d %H:%M %p (CST)')
-#         text += date + '  -  ' + row['coin'] + '  -  ' + row['signal'] + '  -  '  + str(row['price']) +  '\n\n'
-#
-#     old_signal_df = old_signal_df.append(signal_df, ignore_index=True, sort=False)
-#     old_signal_df.to_csv('../signals.csv', index=False)
+    signal_df = pd.DataFrame(signal_df, columns=['date', 'signal', 'coin', 'price'])
+    signal_df = signal_df.sort_values('date').reset_index(drop=True)
 
-    # requests.get(url + urlencode({'chat_id': chat_id, 'text': text}))
+    old_signal_df = pd.read_csv('../signals/' + candle_string + '.csv')
+    new_signals = signal_df.loc[signal_df['date'] > old_signal_df['date'].iloc[-1]]
+
+    if len(new_signals) > 0:
+        for _, row in new_signals.iterrows():
+            date = row['date'].strftime('%m/%d %I:%M %p (CST)')
+            text += date + '  -  ' + row['coin'] + '  -  ' + row['signal'] + '  -  '  + str(row['price']) +  '\n\n'
+
+        old_signal_df = old_signal_df.append(signal_df, ignore_index=True, sort=False)
+        old_signal_df.to_csv('../signals/' + candle_string + '.csv', index=False)
+
+        requests.get(url + urlencode({'chat_id': chat_id, 'text': text}))
--- py/variables.py
+++ py/variables.py
@@ -3,12 +3,17 @@ import pandas as pd
 from datetime import datetime, timedelta
 import time
 
-bitmex = ccxt.bitmex()
+binance = ccxt.binance()
 
 since = datetime.now() - timedelta(hours=500)
-since = time.mktime(since.timetuple())*1000
+since = int(time.mktime(since.timetuple())*1000)
 
-tickers = ['BTC/USD', 'ETH/USD', 'ETHU19', 'XRPU19', 'LTCU19', 'EOSU19']
+# tickers = ['BTC/USD', 'ETH/USD', 'ETHU19', 'BCHU19', 'XRPU19', 'LTCU19', 'EOSU19']
+tickers = ['BTC/USDT', 'ETH/USDT', 'ETH/BTC', 'BCH/BTC', 'XRP/BTC', 'LTC/BTC', 'EOS/BTC']
+candle_intervals = {
+    '1h': 'Hourly',
+    '1d': 'Daily'
+}
 
 API = '862004249:AAFS3xQAWRCYVbadZqr94k3sA5oqyjzmMW8'
 url = 'https://api.telegram.org/bot' + API + '/sendMessage?'
