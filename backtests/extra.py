073914862a585d2d82deeee6007990f8c971cac1
Carter Carlson <carlfarterson@gmail.com>
2019-08-13 15:13:09 -0500

    Move performance comparison to extra.py

--- backtests/extra.py
+++ backtests/extra.py
@@ -33,3 +33,42 @@ for i, row in df[13165:].iterrows():
 
     if result == 2:
         SL = purchase_price
+
+
+
+# Comparing performance of different candle periods to the hourly period
+windows = [4, 6, 12, 24, 36]
+
+def correct(window, row):
+    if row['signal'] == 'BUY':
+        if row['close_' + str(window)] > row['close']:
+            return True
+        else:
+            return False
+    else:  # row['signal'] == 'SELL'
+        if row['close_' + str(window)] < row['close']:
+            return True
+        else:
+            return False
+
+
+
+coin = 'BTC'
+df_all = pd.read_csv('prices/' + coin + '.csv')
+
+df = df_all.dropna().copy()
+results = pd.DataFrame()
+
+for window in windows:
+    df['close_' + str(window)] = [group_candles(df_all[i:i+window])[4] for i in df.index]
+    df['close_'+str(window)+'_delta'] = [(row['close_'+str(window)] - row['close']) / row['close'] for _, row in df.iterrows()]
+    results[window] = [correct(window, row) for _, row in df.iterrows()]
+
+
+df.drop(['open', 'high', 'low', 'volume'], axis=1, inplace=True)
+
+cols = [col for col in df.columns if 'delta' in col]
+
+df.groupby('signal')[cols].mean()
+
+df.head(20)
