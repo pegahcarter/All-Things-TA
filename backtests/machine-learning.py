36792f06f1667b35d89daea4980ba2acb1cf0c3a
Carter Carlson <carlfarterson@gmail.com>
2019-08-28 13:57:39 -0500

    Add loop to test signals from all currency pairs

--- backtests/machine-learning.py
+++ backtests/machine-learning.py
@@ -2,44 +2,49 @@ import numpy as np
 import pandas as pd
 from py.functions import *
 
-df = pd.read_csv('ohlcv/BTC.csv')
-df['ma20'] = df['close'].rolling(window=20).mean().fillna(0)
-df['ema40'] = df['close'].ewm(span=40, adjust=False).mean()
+btc = pd.read_csv('ohlcv/BTC.csv')
+signals = pd.DataFrame()
+coins = ['BTC', 'BCH', 'BNB', 'EOS', 'ETH', 'LTC', 'XRP']
+features = [ 'ma20_slope', 'ma20_slope_direction', 'ema40_slope', 'ema40_slope_direction', 'ma20_ema40_same_direction', 'ma20_ema40_diff']
 
-df['ma20_slope'] = np.subtract(df['ma20'][1:], df['ma20'][:-1]) / df['ma20'][:-1] * 100
-df['ma20_slope_direction'] = df['ma20_slope'] > 0
-df['ema40_slope'] = np.subtract(df['ema40'][1:], df['ema40'][:-1]) / df['ema40'][:-1] * 100
-df['ema40_slope_direction'] = df['ema40_slope'] > 0
-df['ma20_ema40_same_direction'] = df['ma20_slope_direction'] == df['ema40_slope_direction']
-df['ma20_ema40_diff'] = np.subtract(df['ma20'], df['ema40']) / df['ma20']
+for coin in coins:
+    for price in ['USD', 'BTC']:
+        df = pd.read_csv('ohlcv/' + coin + '.csv')
+        if coin == 'BTC' and price == 'BTC':
+            continue
+        elif price == 'BTC':
+            for col in ['open', 'high', 'low', 'close']:
+                df[col] /= btc[col]
 
-signals = find_signals(df)
-signals['tp'] = determine_TP(df, signals, cushion=0.003)
+        df['ma20'] = df['close'].rolling(window=20).mean().fillna(0)
+        df['ema40'] = df['close'].ewm(span=40, adjust=False).mean()
 
-signals['ma20_slope'] = None
-signals['ma20_slope_direction'] = None
-signals['ema40_slope'] = None
-signals['ema40_slope_direction'] = None
-signals['ma20_ema40_same_direction'] = None
-signals['ma20_ema40_diff'] = None
+        df['ma20_slope'] = np.subtract(df['ma20'][1:], df['ma20'][:-1]) / df['ma20'][:-1] * 100
+        df['ma20_slope_direction'] = df['ma20_slope'] > 0
+        df['ema40_slope'] = np.subtract(df['ema40'][1:], df['ema40'][:-1]) / df['ema40'][:-1] * 100
+        df['ema40_slope_direction'] = df['ema40_slope'] > 0
+        df['ma20_ema40_same_direction'] = df['ma20_slope_direction'] == df['ema40_slope_direction']
+        df['ma20_ema40_diff'] = np.subtract(df['ma20'], df['ema40']) / df['ma20']
 
-cols = [
-    'ma20_slope',
-    'ma20_slope_direction',
-    'ema40_slope',
-    'ema40_slope_direction',
-    'ma20_ema40_same_direction',
-    'ma20_ema40_diff'
-]
+        coin_signals = find_signals(df)
+        coin_signals['tp'] = determine_TP(df, coin_signals, cushion=0.003)
 
-for i in signals.index:
-    signals.loc[i, cols] = df.loc[i, cols]
+        for feature in features:
+            coin_signals[feature] = None
+
+        for i in coin_signals.index:
+            coin_signals.loc[i, features] = df.loc[i, features]
+
+        coin_signals['ticker'] = [coin + '/' + price for i in range(len(coin_signals))]
+
+        signals = signals.append(coin_signals, ignore_index=True, sort=False)
+
 
 from sklearn.ensemble import RandomForestClassifier
 import matplotlib.pyplot as plt
 tree = RandomForestClassifier()
-x = signals[cols]
-y = signals['tp'] == 4
+x = signals[['ema40_slope', 'ma20_slope', 'ma20_ema40_diff']]
+y = signals['tp'] > 0
 
 tree.fit(x, y)
 feature_importance = tree.feature_importances_
