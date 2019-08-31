89f55089cbcbb7dfda04ff49c1076b4cd195a1c6
Carter Carlson <carlfarterson@gmail.com>
2019-08-30 21:23:42 -0500

    Finish basic loop for compounding.  Few wrinkles left to iron out.

--- backtests/simulations/compounding.py
+++ backtests/simulations/compounding.py
@@ -7,47 +7,55 @@ import pandas as pd
 from py.functions import *
 from backtests.simulations.portfolio import Portfolio
 
-df = pd.read_csv('ohlcv/ETH.csv')
-signals = find_signals(df)
+tp_pcts = [-1, .05, .15, .35, 2.45]
+signals = pd.DataFrame()
 
-# Step 1: figure out how long the position is open
-tp, index_tp_hit = determine_TP(df, signals, compound=True)
-signals = signals.reset_index()
-signals['tp'] = tp
-signals['hrs_position_open'] = np.subtract(index_tp_hit, signals['index'])
-signals['index_closed'] = index_tp_hit
+
+for coin in ['BTC', 'ETH', 'LTC']:
+    ticker = coin + '/USD'
+    df = pd.read_csv('ohlcv/' + coin + '.csv')
+    coin_signals = find_signals(df)
+    tp, index_tp_hit = determine_TP(df, coin_signals, compound=True)
+    coin_signals['tp'] = tp
+    coin_signals['index_tp_hit'] = index_tp_hit
+    coin_signals['ticker'] = [coin + '/USD' for i in range(len(coin_signals))]
+    coin_signals = coin_signals.reset_index()
+
+    profit_pct = abs(coin_signals['price'] - coin_signals['stop_loss']) / coin_signals['price']
+    end_pct = list(map(lambda x: tp_pcts[x], coin_signals['tp']))
+    coin_signals['net_profit'] = profit_pct * end_pct
+
+    signals = signals.append(coin_signals, ignore_index=True, sort=False)
+
+# Sort signals by date
+signals = signals.sort_values('date').reset_index(drop=True)
 
 # Step 2: figure out the most # of positions open at one time
+signals['hrs_position_open'] = np.subtract(signals['index_tp_hit'], signals['index'])
 signals['total_positions_open'] = [sum(signals['index'][i] + signals['hrs_position_open'][i] > signals['index'][i:]) for i in signals.index]
+# list(signals['total_positions_open']).index(signals['total_positions_open'].max())
 
-# Step 3: figure out net profit
-tp_pcts = [-1, .05, .15, .35, 2.45]
-profit_pct = abs(signals['price'] - signals['stop_loss']) / signals['price']
-end_pct = list(map(lambda x: tp_pcts[x], signals['tp']))
-signals['net_profit'] = profit_pct * end_pct
-
-# Step 4: simulate trading the first signal
-signals.head()
-
-ticker = 'ETH/USD'
 portfolio = Portfolio()
-
 for i in range(len(df)):
-    if i in signals.index:
-        portfolio.open_position(ticker, **dict(signals.iloc[list(signals.index).index(i)]))
-    elif len(portfolio.positions[ticker]) == 0:
+    if i in signals['index']:
+        for _, position in signals[signals['index'] == i].iterrows():
+            portfolio.open_position(**position)
+    elif len(portfolio.positions) == 0:
         continue
 
-    if i in map(lambda x: x['index_closed'], portfolio.positions[ticker]):
-        for position in list(filter(lambda x: x['index_closed'] == i, portfolio.positions[ticker])):
-            portfolio.close_position(ticker, position)
+    if i in map(lambda x: x['index_tp_hit'], portfolio.positions):
+        for position in list(filter(lambda x: x['index_tp_hit'] == i, portfolio.positions)):
+            portfolio.close_position(position)
 
 
+len(df)
+
+signals[10:20]
+
+signals['index'][15] in range(0, 1200)
+
+
+i
 portfolio.positions
 portfolio.available_capital
-
-# ETH/USD (2018.08.30)
-# 99362.49622200451
-
-
-signals
+portfolio.num_positions
--- backtests/simulations/portfolio.py
+++ backtests/simulations/portfolio.py
@@ -8,19 +8,18 @@ class Portfolio:
         self.PORTFOLIO_START_VALUE = 10000.
         self.available_capital = self.PORTFOLIO_START_VALUE
         self.num_positions = 0
-        # self.positions = {ticker: [] for ticker in tickers}
-        self.positions = {'BTC/USD': []}
+        self.positions = []
 
 
-    def open_position(self, ticker, **kwargs):
+    def open_position(self, **kwargs):
         self.num_positions += 1
-        kwargs['d_amt'] = self.available_capital * .25
+        kwargs['d_amt'] = self.available_capital * .01
         self.available_capital -= kwargs['d_amt']
-        self.positions[ticker].append(
+        self.positions.append(
             kwargs
         )
 
 
-    def close_position(self, ticker, position):
-        self.available_capital += position['d_amt']*(position['net_profit'])*10 + position['d_amt']
-        return self.positions[ticker].pop(self.positions[ticker].index(position))
+    def close_position(self, position):
+        self.available_capital += position['d_amt']*(position['net_profit']*10) + position['d_amt']
+        # return self.positions.pop(self.positions.index(position))
