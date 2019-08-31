0b5de2d3e6ceba4f3b6762145fa934cac8616a49
Carter Carlson <carlfarterson@gmail.com>
2019-08-31 16:55:53 -0500

    Find profit of just USD pairs

--- backtests/machine-learning.py
+++ backtests/machine-learning.py
@@ -11,37 +11,35 @@ features = [ 'ma20_slope', 'ma20_slope_direction', 'ema40_slope', 'ema40_slope_d
 tp_pcts = [-1, 0.05, 0.15, 0.35, 2.45, 0]
 
 for coin in coins:
-    for price in ['USD', 'BTC']:
-        df = pd.read_csv('ohlcv/' + coin + '.csv')
-        if coin == 'BTC' and price == 'BTC':
-            continue
-        elif price == 'BTC':
-            for col in ['open', 'high', 'low', 'close']:
-                df[col] /= btc[col]
+    # for price in ['USD', 'BTC']:
+    df = pd.read_csv('ohlcv/' + coin + '.csv')
+    # if coin == 'BTC' and price == 'BTC':
+    #     continue
+    # elif price == 'BTC':
+    #     for col in ['open', 'high', 'low', 'close']:
+    #         df[col] /= btc[col]
 
-        df['ma20'] = df['close'].rolling(window=20).mean().fillna(0)
-        df['ema40'] = df['close'].ewm(span=40, adjust=False).mean()
+    df['ma20'] = df['close'].rolling(window=20).mean().fillna(0)
+    df['ema40'] = df['close'].ewm(span=40, adjust=False).mean()
 
-        df['ma20_slope'] = np.subtract(df['ma20'][1:], df['ma20'][:-1]) / df['ma20'][:-1] * 100
-        df['ma20_slope_direction'] = df['ma20_slope'] > 0
-        df['ema40_slope'] = np.subtract(df['ema40'][1:], df['ema40'][:-1]) / df['ema40'][:-1] * 100
-        df['ema40_slope_direction'] = df['ema40_slope'] > 0
-        df['ma20_ema40_same_direction'] = df['ma20_slope_direction'] == df['ema40_slope_direction']
-        df['ma20_ema40_diff'] = np.subtract(df['ma20'], df['ema40']) / df['ma20']
+    df['ma20_slope'] = np.subtract(df['ma20'][1:], df['ma20'][:-1]) / df['ma20'][:-1] * 100
+    df['ma20_slope_direction'] = df['ma20_slope'] > 0
+    df['ema40_slope'] = np.subtract(df['ema40'][1:], df['ema40'][:-1]) / df['ema40'][:-1] * 100
+    df['ema40_slope_direction'] = df['ema40_slope'] > 0
+    df['ma20_ema40_same_direction'] = df['ma20_slope_direction'] == df['ema40_slope_direction']
+    df['ma20_ema40_diff'] = np.subtract(df['ma20'], df['ema40']) / df['ma20']
 
-        coin_signals = find_signals(df)
-        coin_signals['tp'] = determine_TP(df, coin_signals, cushion=0.003)
+    coin_signals = find_signals(df)
+    coin_signals['tp'] = determine_TP(df, coin_signals, cushion=0.003)
 
-        for feature in features:
-            coin_signals[feature] = None
+    for feature in features:
+        coin_signals[feature] = None
 
-        for i in coin_signals.index:
-            coin_signals.loc[i, features] = df.loc[i, features]
+    for i in coin_signals.index:
+        coin_signals.loc[i, features] = df.loc[i, features]
 
-        coin_signals['ticker'] = [coin + '/' + price for i in range(len(coin_signals))]
-
-        signals = signals.append(coin_signals, ignore_index=True, sort=False)
-
+    coin_signals['ticker'] = [coin + '/USD' for i in range(len(coin_signals))]
+    signals = signals.append(coin_signals, ignore_index=True, sort=False)
 
 
 # tree = RandomForestClassifier()
@@ -59,8 +57,6 @@ for coin in coins:
 # plt.yticks(pos, x.columns[feature_sorted])
 # plt.show()
 
-
-
 # ------------------------------------------------------------------------------
 
 # Testing out eliminating trades based on slope
@@ -70,10 +66,13 @@ signals['end_pct'] = list(map(lambda x: tp_pcts[x], signals['tp']))
 signals['net_profit'] = signals['end_pct'] * signals['profit_pct']
 signals['net_profit'].sum()
 # W/ only 4% limit on SL
-# 6.835608249537152
+# 6.8356
 
 # W/ 5% limit on SL and high compared to price
-# 8.189711112781826
+# 8.19
+
+# W/ 5% ... & at least .75% between price and SL
+# 7.532
 
 signals.groupby('tp')['tp'].count()
 len(signals)
