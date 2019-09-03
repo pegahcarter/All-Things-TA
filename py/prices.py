ce6b0d3c3580f5b9be4587ed8a6119b22514ab2a
Carter Carlson <carlfarterson@gmail.com>
2019-09-03 13:07:08 -0500

    Update prices with current data

--- py/prices.py
+++ py/prices.py
@@ -3,26 +3,34 @@ import pandas as pd
 import numpy as np
 import ccxt
 from datetime import datetime, timedelta
-from time import sleep
+import time
 
+binance = ccxt.binance()
 
 btc = pd.read_csv('ohlcv/BTC.csv')
-binance = ccxt.binance()
-start = datetime(year=2018,month=1,day=1)
-
-# for coin in ['ETH' ,'BCH', 'XRP', 'BCH', 'LTC']:
+start = datetime.strptime(btc['date'].iat[-1], '%Y-%m-%d %H:%M:%S')
 start_date = start
-df = []
-while start_date < datetime.now():
-    data = binance.fetch_ohlcv('ADA/BTC', '1h', limit=500, since=int(start_date.timestamp()*1000))
-    df += data
-    start_date += timedelta(hours=len(data))
-    sleep(1)
 
-df = pd.DataFrame(df, columns=['date', 'open', 'high', 'low', 'close', 'volume'])
-df['date'] = [datetime.fromtimestamp(x/1000) for x in df['date']]
 
-for col in ['open', 'high', 'low', 'close', 'volume']:
-    df[col] *= btc['close']
+for ticker in ['BTC/USDT', 'ADA/BTC', 'EOS/BTC', 'ETH/BTC', 'LTC/BTC', 'XRP/BTC']:
+    coin = ticker[:ticker.find('/')]
+    start_date = start
+    df = []
 
-df.dropna().to_csv('ohlcv/' + coin + '.csv', index=False)
+    while start_date < datetime.now():
+        data = binance.fetch_ohlcv(ticker, '1h', limit=500, since=int(time.mktime(start_date.timetuple())*1000))
+        df += data
+        start_date += timedelta(hours=len(data))
+        time.sleep(1)
+
+    df = pd.DataFrame(df, columns=['date', 'open', 'high', 'low', 'close', 'volume'])
+    df['date'] = [datetime.fromtimestamp(x/1000) for x in df['date']]
+    if coin == 'BTC':
+        btc_price = df['close'].copy()
+    else:
+        for col in ['open', 'high', 'low', 'close', 'volume']:
+            df[col] *= btc_price
+
+    df_old = pd.read_csv('ohlcv/' + coin + '.csv')
+    df_new = df_old.append(df, ignore_index=True, sort=False)
+    df_new.to_csv('ohlcv/' + coin + '.csv', index=False)
