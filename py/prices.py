a006b83969d876b486bc778c1d1be0056d39e4ca
Carter Carlson <carlfarterson@gmail.com>
2019-09-02 20:21:47 -0500

    Fix decimals on dates

--- py/prices.py
+++ py/prices.py
@@ -3,22 +3,26 @@ import pandas as pd
 import numpy as np
 import ccxt
 from datetime import datetime, timedelta
+from time import sleep
+
 
 btc = pd.read_csv('ohlcv/BTC.csv')
 binance = ccxt.binance()
 start = datetime(year=2018,month=1,day=1)
 
-for coin in ['ETH' ,'BCH', 'XRP', 'BCH', 'LTC']:
-    start_date = start
-    df = []
-    while start_date < datetime.now():
-        data = binance.fetch_ohlcv(coin + '/BTC', '1h', limit=1000, since=int(start_date.timestamp()*1000))
-        df += data
-        start_date += timedelta(hours=len(data))
+# for coin in ['ETH' ,'BCH', 'XRP', 'BCH', 'LTC']:
+start_date = start
+df = []
+while start_date < datetime.now():
+    data = binance.fetch_ohlcv('ADA/BTC', '1h', limit=500, since=int(start_date.timestamp()*1000))
+    df += data
+    start_date += timedelta(hours=len(data))
+    sleep(1)
 
-    df = pd.DataFrame(df, columns=['date', 'open', 'high', 'low', 'close', 'volume'])
-    df['date'] = [start + timedelta(hours=i) for i in range(len(df))]
-    for col in ['open', 'high', 'low', 'close', 'volume']:
-        df[col] *= btc['close']
-        
-    df.to_csv('ohlcv/' + coin + '.csv', index=False)
+df = pd.DataFrame(df, columns=['date', 'open', 'high', 'low', 'close', 'volume'])
+df['date'] = [datetime.fromtimestamp(x/1000) for x in df['date']]
+
+for col in ['open', 'high', 'low', 'close', 'volume']:
+    df[col] *= btc['close']
+
+df.dropna().to_csv('ohlcv/' + coin + '.csv', index=False)
