7b57296ed62cd735dbf8db51825f8b87550cf833
Carter Carlson <carlfarterson@gmail.com>
2019-09-22 19:17:26 -0500

    Update OHLCV data and succesfully merged backtests with real signals

--- py/merge-signals.py
+++ py/merge-signals.py
@@ -1,55 +1,58 @@
 # Merges real backtest signals with actual sent signals
 import pandas as pd
-
-# NOTE: this takes backtests.csv signals as previous signals
-# signals = pd.read_csv('ohlcv/WORLD CLASS TRADERS BACKTEST RESULTS.csv')
-signals = pd.read_csv('ohlcv/backtests.csv')
-
-signals_clean = signals.drop(['index', 'tp', 'index_closed', 'net_profit', 'hrs_open'], axis=1)
+from py.functions import *
+from datetime import datetime, timedelta
 
 real_signals = pd.read_csv('signals/Hourly.csv')
-old_signals = signals_clean[signals_clean['date'] < real_signals['date'][0]]
+backtest_signals = pd.read_csv('ohlcv/backtests.csv', usecols=real_signals.columns)
 
+old_signals = backtest_signals[backtest_signals['date'] < real_signals['date'][0]]
 merged_signals = old_signals.append(real_signals, sort=False).reset_index(drop=True)
 
+# Save merged signals
 merged_signals.to_csv('signals/Hourly-merged.csv', index=False)
-
-
-btc = pd.read_csv('ohlcv/BTC.csv')
-dates = btc['date'].tolist()
-
-merged_signals.index = merged_signals['date'].apply(lambda x: dates.index(x))
 merged_signals['tp'] = None
 merged_signals['index_closed'] = None
 
+btc = pd.read_csv('ohlcv/BTC.csv')
+signals = pd.DataFrame()
+
+
 for ticker in set(merged_signals['ticker']):
     coin = ticker[:ticker.find('/')]
-    df = pd.read_csv('ohlcv/' + coin + '.csv')
-    coin_signals = merged_signals.loc[merged_signals['ticker'] == ticker]
 
-    if '/BTC' in ticker:
+    df = pd.read_csv('ohlcv/' + coin + '.csv')
+    dates = df['date'].tolist()
+
+    coin_signals = merged_signals.loc[merged_signals['ticker'] == ticker]
+    coin_signals.index = [dates.index(i) for i in coin_signals['date']]
+
+    if '/BTC' in ticker and ticker != 'BCH/BTC':
         for col in ['open', 'high', 'low', 'close']:
             df[col] /= btc['close']
 
     tp, index_closed = determine_TP(df, coin_signals, compound=True)
     coin_signals['tp'] = tp
     coin_signals['index_closed'] = index_closed
+    coin_signals.reset_index(inplace=True)
+
+    signals = signals.append(coin_signals, ignore_index=True, sort=False)
 
 
+tp_pcts = [-1, .05, .15, .35, 2.45]
+profit_pct = abs(signals['price'] - signals['stop_loss']) / signals['price']
+signals['tp'] = signals['tp'].astype('int')
+end_pct = list(map(lambda x: tp_pcts[x], signals['tp']))
+signals['net_profit'] = end_pct * profit_pct
 
+signals['date'] = signals['date'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
+hrs_open = signals['index_closed'] - signals['index']
+signals['date_closed'] = [date + timedelta(seconds=hrs*60*60) for date, hrs in zip(signals['date'], hrs_open)]
 
+signals = signals\
+            .sort_values('date')\
+            .reset_index(drop=True)\
+            .drop(['index', 'index_closed'], axis=1)\
+            .rename(columns={'date': 'date_opened'})
 
-
-
-
-test.head()
-
-
-
-
-
-
-
-
-
-coin_signals = merged_signals[merged_signals['ticker'] == 'BTC/USD']
+signals.to_csv('ohlcv/WORLD CLASS TRADERS BACKTEST RESULTS.csv', index=False)
--- py/save_trades.py
+++ py/save_trades.py
@@ -1,19 +1,21 @@
 import pandas as pd
 from py.functions import *
+from datetime import datetime, timedelta
 
 btc = pd.read_csv('ohlcv/BTC.csv')
 signals = pd.DataFrame()
 
 
-for ticker in ['BTC/USD', 'ETH/USD', 'ETH/BTC', 'LTC/BTC', 'EOS/BTC', 'XRP/BTC', 'ADA/BTC']:
+for ticker in ['BTC/USD', 'ETH/USD', 'ETH/BTC', 'LTC/BTC', 'EOS/BTC', 'XRP/BTC', 'BCH/BTC']:
     coin = ticker[:ticker.find('/')]
     df = pd.read_csv('ohlcv/' + coin + '.csv', usecols=['date', 'open', 'high', 'low', 'close'])
 
-    if '/BTC' in ticker:
+    if '/BTC' in ticker and ticker != 'BCH/BTC':
         for col in ['open', 'high', 'low', 'close']:
             df[col] /= btc['close']
 
     coin_signals = find_signals(df)
+    # tp, index_closed = determine_TP(df, coin_signals, compound=True)
     tp, index_closed = determine_TP(df, coin_signals, compound=True)
     coin_signals['tp'] = tp
     coin_signals['index_closed'] = index_closed
@@ -30,8 +32,10 @@ signals['tp'] = signals['tp'].astype('int')
 end_pct = list(map(lambda x: tp_pcts[x], signals['tp']))
 signals['net_profit'] = end_pct * profit_pct
 
-signals = signals.sort_values('date')
-signals['hrs_open'] = signals['index_closed'] - signals['index']
+signals['date'] = signals['date'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
+hrs_open = signals['index_closed'] - signals['index']
+signals['date_closed'] = [date + timedelta(seconds=hrs*60*60) for date, hrs in zip(signals['date'], hrs_open)]
 
-# signals.to_csv('ohlcv/WORLD CLASS TRADERS BACKTEST RESULTS.csv', index=False)
+signals = signals.sort_values('date').reset_index(drop=True)
+
 signals.to_csv('ohlcv/backtests.csv', index=False)
