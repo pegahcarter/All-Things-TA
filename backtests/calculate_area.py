94cb404e0dedb780108bee12c0841c1679797a2e
Carter Carlson <carlfarterson@gmail.com>
2019-09-29 00:28:49 -0400

    Get to step 6

--- backtests/calculate_area.py
+++ backtests/calculate_area.py
@@ -24,16 +24,13 @@ num = dap.dot(dp)
 # ------------------------------------------------------------------------------
 # Step 2. Use the same 4 points, similar to two points on moving averages that cross
 
-
 line3 = np.array([5, 8])
 line4 = np.array([6, 2])
+# cross_indices = crossover(line3, line4)
 
 plt.plot(line3)
 plt.plot(line4)
 plt.show()
-
-# intersection(line3, line4)
-cross_indices = crossover(line3, line4)
 
 # VERSION 1
 test = list(map(lambda x: [[line3[x-1], line3[x]], [line4[x-1], line4[x]]], cross_indices))
@@ -46,33 +43,105 @@ matrices = test[0]
 
 da = [1, matrices[0][1] - matrices[0][0]]
 db = [1, matrices[1][1] - matrices[1][0]]
+dp = [0, matrices[0][0] - matrices[1][0]]
+
+dap = [-da[1], da[0]]
+# OR
+dap = [-da[1], 1]
+
+denom = np.dot(dap, db)
+# OR
+denom = dap[0]*db[0] + dap[1]*db[1]
 
 
+num = np.dot(dap, dp)
+# OR
+num = dap[0]*dp[0] + dap[1]*dp[1]
 
+(float(num) / denom)*np.array(db) + [0, matrices[1][0]]
 
+# ------------------------------------------------------------------------------
+# Step 3. Simplify steps above
 
+line3 = np.array([5, 8])
+line4 = np.array([6, 2])
 
+cross_indices = crossover(line3, line4)
 
+test = list(map(lambda x: [[line3[x-1], line3[x]], [line4[x-1], line4[x]]], cross_indices))
 
+matrices = test[0]
 
+dap = [matrices[0][0] - matrices[0][1], 1]
 
+denom = dap[0] + (matrices[1][1] - matrices[1][0])
 
+num = matrices[0][0] - matrices[1][0]
 
+float(num) / denom * np.array([1, matrices[1][1] - matrices[1][0]]) + [0, matrices[1][0]]
+# ------------------------------------------------------------------------------
+# Step 4a. Fill in numbers with variables in step 3 for further clarification
 
+line3 = np.array([5, 8])
+line4 = np.array([6, 2])
 
+[[5, 8], [6, 2]]
 
+dap = [8 - 5, 1]
 
+denom = (5 - 8) + (2 - 6)
 
+num = 5 - 6
 
+float(num) / denom * np.array([1, 2 - 6]) + [0, 6]
 
+# ------------------------------------------------------------------------------
+# Step 4b. Another take on 4a
 
+line3 = [5, 8]
+line4 = [6, 2]
 
+denom = (line3[0] - line3[1]) + (line4[1] - line4[0])
+num = line3[0] - line4[0]
 
+float(num) / denom * np.array([1, line4[1] - line4[0]]) + [0, line4[0]]
 
+# ------------------------------------------------------------------------------
+# Step 5. Simplify the last step
 
+multiplier = float(num) / denom
+[multiplier, multiplier * (line4[1] - line4[0]) + line4[0]]
 
+# Right side
+multiplier*(line4[1] - line4[0]) + line4[0]
 
+x = multiplier
+a = line4[0]
+b = line4[1]
 
+a + x*(b - a)
+
+# ------------------------------------------------------------------------------
+# Step 6. Figure out less repeated calculations
+
+line3_diff = line3[1] - line3[0]
+line4_diff = line4[1] - line4[0]
+
+# Re-order denom
+denom = (line3[0] - line3[1]) + (line4[1] - line4[0])
+# goes to...
+denom = (line4[1] - line4[0]) - (line3[1] - line3[0])
+# goes to...
+denom = line4_diff - line3_diff
+
+pos1_diff = float(line3[0] - line4[0])
+
+# NOTE: `x` is the same as slope
+x = (pos1_diff)/(line4_diff - line3_diff)
+
+y = line4_diff * x + line4[0]
+
+intercept = [x, y]
 
 
 
