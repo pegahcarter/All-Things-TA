2d2a946ae1ae520c8929b1703895cd6d5a299624
Carter Carlson <carlfarterson@gmail.com>
2019-10-25 01:18:28 +0000

    Update file references to run code on remote server

--- py/main.py
+++ py/main.py
@@ -9,12 +9,12 @@ def main():
 
         signal_df = logic.run(averages)
 
-        old_signal_df = pd.read_csv('C:/Users/carter/Documents/crypto/peter-signal/data/signals/' + channel + '.csv')
+        old_signal_df = pd.read_csv('/home/carter/peter-signal/data/signals/' + channel + '.csv')
         new_signals = signal_df[signal_df['date'] > max(old_signal_df['date'])]
 
         if len(new_signals) > 0:
             old_signal_df = old_signal_df.append(new_signals, ignore_index=True, sort=False)
-            old_signal_df.to_csv('C:/Users/carter/Documents/crypto/peter-signal/data/signals/' + channel + '.csv', index=False)
+            old_signal_df.to_csv('/home/carter/peter-signal/data/signals/' + channel + '.csv', index=False)
 
             for _, row in new_signals.iterrows():
                 logic.send_signal(row, channel)
