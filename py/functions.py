74dd0c5dffb31cdcb609f57d9f430fc3d9201e75
Carter Carlson <carlfarterson@gmail.com>
2019-11-27 14:54:35 -0600

    Convert signals object from dataframe to list of dicts

--- py/functions.py
+++ py/functions.py
@@ -20,7 +20,7 @@ def find_signals(df, window_fast, window_mid, window_slow):
     high = df['high'].tolist()
     low = df['low'].tolist()
 
-    signals = {}
+    signals = []
 
     for i in crossover(emafast, mamid):
         if i < 48:
@@ -39,35 +39,35 @@ def find_signals(df, window_fast, window_mid, window_slow):
         signal = None
 
         if price > emafast[i]:
-            if mamid[i] > emaslow[i] and relative_strength[i] > 50 and price > mabase[i]:
+            if price > mabase[i] and mamid[i] > emaslow[i] and relative_strength[i] > 50:
                 signal = 'long'
                 stop_loss = min(low[i-10:i])
         else:   # price < emafast[i]
-            if mamid[i] < emaslow[i] and relative_strength[i] < 50 and price < mabase[i]:
+            if price < mabase[i] and mamid[i] < emaslow[i] and relative_strength[i] < 50:
                 signal = 'short'
                 stop_loss = max(high[i-10:i])
 
-        if signal and 0.0075 < abs(1 - stop_loss/price) < .04 and mamid_emaslow_diff[i] > .001:
-            signals[i] = {'date': df['date'][i],
-                          'signal': signal,
-                          'price': price,
-                          'stop_loss': stop_loss}
+        if signal and mamid_emaslow_diff[i] > .001 and 0.0075 < abs(1 - stop_loss/price) < .04:
+            signals.append({'index_opened': i,
+                            'date': df['date'][i],
+                            'signal': signal,
+                            'price': price,
+                            'stop_loss': stop_loss,
+                            'pct': abs(stop_loss - price) / price})
 
-    return pd.DataFrame.from_dict(signals, orient='index')
+    return signals
+    # return pd.DataFrame.from_dict(signals, orient='index')
 
 
 def determine_TP(df, signals, cushion=0):
     ''' Figure out which TP level is hit '''
-
-    tp_lst = []
-    index_tp_hit_lst = []
 
     low = df['low'].tolist()
     low_inverse = (-df['low']).tolist()
     high = df['high'].tolist()
     high_inverse = (-df['high']).tolist()
 
-    for index, row in signals.iterrows():
+    for i, row in enumerate(signals):
         price = row['price']
         stop_loss = row['stop_loss']
         if row['signal'] == 'long':
@@ -89,10 +89,10 @@ def determine_TP(df, signals, cushion=0):
         tp4 = price + diff*3
 
         tp_targets = [tp1, tp2, tp3, tp4]
-        index_tp_hit = [0, 0, 0, 0]
+        index_tp_hit = [None, None, None, None]
         tp = 0
 
-        for x in range(index+1, len(df)):
+        for x in range(row['index_opened'] + 1, len(df)):
             while tp != 4 and u_bounds[x] > tp_targets[tp]:
                 index_tp_hit[tp] = x
                 tp += 1
@@ -101,11 +101,9 @@ def determine_TP(df, signals, cushion=0):
             if tp > 0:
                 stop_loss = price
 
-        tp_lst.append(tp)
-        index_tp_hit_lst.append(index_tp_hit)
-
-    return tp_lst, index_tp_hit_lst
-
+        signals[i]['tp'] = tp
+        signals[i]['index_tp_hit'] = index_tp_hit
+        signals[i]['index_closed'] = x
 
 
 # ------------------------------------------------------------------------------
