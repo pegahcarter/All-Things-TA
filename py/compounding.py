c6029c73151fa2d60ec3a11bc32b9232f139afaa
Carter Carlson <carlfarterson@gmail.com>
2019-12-02 17:06:37 -0600

    Add portfolio class to test

--- py/compounding.py
+++ py/compounding.py
@@ -12,8 +12,12 @@ for f in os.listdir('../data/binance/'):
     # Add `tp`, `index_tp_hit`, and `index_closed`
     determine_TP(df, coin_signals)
 
-    # Add ticker to signal
-    [x.update({'ticker': f[:f.find('.')]}) for x in coin_signals]
+    # Add ticker & pct_open to signal
+    for x in coin_signals:
+        x.update({
+            'ticker': f[:f.find('.')],
+            'pct_open': 100
+        })
 
     # Add coin signals to the primary signal list
     signals.extend(coin_signals)
@@ -21,18 +25,59 @@ for f in os.listdir('../data/binance/'):
 # Re-order signals by `index_opened`
 signals_sorted = list(sorted(signals, key=lambda x: x['index_opened']))
 
-signal = signals_sorted[0]
+class Portfolio:
 
-#
+    initial_capital = 10000
+    x_leverage = 1
+    trade_size = .1
 
-signal
+    def __init__(self, *args, **kwargs):
+        self.available_capital = self.initial_capital
+        self.positions = []
+        self.index_tp_hit_set = set()
+        self.index_closed_set = set()
+
+    def positions_open(self):
+        return len(self.positions)
+
+    def open_position(self, **position):
+        position['d_amt'] = self.available_capital * self.trade_size * self.x_leverage
+        self.available_capital -= position['d_amt']
+
+        self.index_tp_hit_set.update(position['index_tp_hit'])
+        self.index_closed_set.add(position['index_closed'])
+        self.positions.append(position)
+
+    def close_position(self, x_leverage, **position):
+        self.available_capital += position['d_amt'] * (1 + position['net_profit'] * x_leverage)
+        return self.positions.pop(self.positions.index(position))
+
+
+
+p = Portfolio()
+
+# Add test position
+p.open_position(**signals_sorted[0])
+p.open_position(**signals_sorted[1])
+p.positions
+
+
+list(filter(lambda x: 68 in x['index_tp_hit'], p.positions))
+
+# Sell 10% of position at TP1
+# if 68 in p.index_tp_hit_set:
+
+# TODO: remove [0] from line below when looping through entire portfolio, and
+#   rename `p` as `positions_to_sell`
+p = list(filter(lambda x: 68 in x['index_tp_hit'], p.positions))[0]
+
+# for p in positions_to_sell:
+while 68 in p['index_tp_hit']gfh
 
 
 
 
 
-
-portfolio = Portfolio()
 
 # Old code
 # for i, date in enumerate(btc['date']):
