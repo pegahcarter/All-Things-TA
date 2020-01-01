0ece6903a7c71db7b3c5093dd35dc7b4e0dc52df
Carter Carlson <carlfarterson@gmail.com>
2019-12-31 20:17:36 -0600

    Finish testing Portfolio with new methods and run first loop of results

--- py/compounding-1.py
+++ py/compounding-1.py
@@ -1,12 +1,16 @@
 # Finds compounding results with different moving averages and tp_pct
 from functions import *
 from portfolio import Portfolio
-from variables import avgs_combined, tp_pcts_lst
+from variables import avgs_combined, tp_pcts_list
+import itertools
+import os
+import ujson
 
-results = pd.DataFrame(index=['-'.join(str(x) for x in tp_pcts.values()) for tp_pcts in tp_pcts_lst])
+
+results = pd.DataFrame(index=['-'.join(str(int(x*100)) for x in tp_pcts[1:]) for tp_pcts in tp_pcts_list])
 
 
-for avgs in avgs_combined[:1]:
+for avgs in avgs_combined:
 
     signals = []
     for f in os.listdir('../data/binance/'):
@@ -17,42 +21,49 @@ for avgs in avgs_combined[:1]:
         # Add `tp`, `index_tp_hit`, and `index_closed`
         determine_TP(df, coin_signals)
 
-        # Add ticker & pct_open to signal
+        # Add ticker to signal & remove unused keys
+        ticker = f[:f.find('.')]
         for x in coin_signals:
-            x.update({
-                'ticker': f[:f.find('.')],
-                'pct_open': 100
-            })
+            x.update({'ticker': ticker})
+            x.pop('date')
+            x.pop('price')
+            x.pop('stop_loss')
 
         # Add coin signals to the primary signal list
         signals.extend(coin_signals)
 
-    available_capital_lst = []
-    for tp_pcts in tp_pcts_lst:
+    # Re-order signals by index opened
+    signals = sorted(signals, key=lambda x: x['index_opened'])
 
-        # Re-order signals by `index_opened`
-        signals_sorted = sorted(signals.copy(), key=lambda x: x['index_opened'])
+    indices_tp_hit = set(itertools.chain.from_iterable(map(lambda x: x['index_tp_hit'], signals)))
+    indices_tp_hit.remove(None)
+    indices_opened = set(map(lambda x: x['index_opened'], signals))
+    indices_closed = set(map(lambda x: x['index_closed'], signals))
+    indices_of_action = sorted(indices_opened | indices_tp_hit | indices_closed)
+
+    available_capital_lst = []
+    for tp_pcts in tp_pcts_list:
+
+        signals_copy = ujson.loads(ujson.dumps(signals))
         portfolio = Portfolio(tp_pcts)
 
-        for hr in range(16000):
+        for index_of_action in indices_of_action:
 
             # Opening positions
-            while len(signals_sorted) > 0 and signals_sorted[0]['index_opened'] == hr:
-                portfolio.open_position(signals_sorted.pop(0))
+            while len(signals_copy) > 0 and signals_copy[0]['index_opened'] == index_of_action:
+                portfolio.open(signals_copy.pop(0))
 
-            # Only check for selling/closing positions when we have open trades
-            if len(portfolio.positions) > 0:
+            # Selling positions
+            if index_of_action in indices_tp_hit:
+                for position in filter(lambda x: index_of_action in x['index_tp_hit'], portfolio.positions):
+                    while index_of_action in position['index_tp_hit']:
+                        portfolio.sell(position, index_of_action)
 
-                # Selling part of positions
-                if hr in portfolio.index_tp_hit_set:
-                    for position in filter(lambda x: hr in x['index_tp_hit'], portfolio.positions):
-                        while hr in position['index_tp_hit']:
-                            portfolio.sell_position(hr, position)
+            # Closing positions
+            if index_of_action in indices_closed:
+                for position in list(filter(lambda x: index_of_action == x['index_closed'], portfolio.positions))[::-1]:
+                    portfolio.close(position)
 
-                # Closing out positions
-                if hr in portfolio.index_closed_set:
-                    for position in list(filter(lambda x: hr == x['index_closed'], portfolio.positions))[::-1]:
-                        portfolio.close_position(position)
 
         # Save ending portfolio value
         available_capital_lst.append(portfolio.available_capital)
--- py/portfolio.py
+++ py/portfolio.py
@@ -7,66 +7,64 @@ class Portfolio:
     x_leverage = 10
     trade_size = .1
     # NOTE: rr = risk/return
-    rr_list = [-1, .5, 1, 2, 3]
+    rrs = [-1, .5, 1, 2, 3]
     initial_capital = 10000
 
     def __init__(self, tp_pcts):
         self.available_capital = self.initial_capital
+        self.fees = 0
         self.positions = []
         self.tp_pcts = tp_pcts
+        self.tps = { tp: {'rr': rr, 'pct_sold': pct_sold} \
+                     for tp, (pct_sold, rr) in enumerate(zip(tp_pcts, self.rrs))
+                    }
 
 
-    def _add_fee(self, ticker, trade_value, side, profit=0):
+    def _add_fee(self, ticker, trade_value, profit, signal, method):
 
         if ticker == 'ETH-USDT' or ticker == 'BTC-USDT':
-            if side == 'buy':
+            if (signal == 'long' and method == 'buy') or (signal == 'short' and method == 'sell'):
                 rate = .00075
-            else:  # side == 'sell'
+            else:  # (signal == 'long' and method == 'sell') or (signal == 'short' and method == 'buy'):
                 rate = -.00025
         else:    # ticker is not a perpetual contract
-            if side == 'buy':
+            if (signal == 'long' and method == 'buy') or (signal == 'short' and method == 'sell'):
                 rate = .0025
-            else:  # side == 'sell'
+            else:  # (signal == 'long' and method == 'sell') or (signal == 'short' and method == 'buy'):
                 rate = -.0005
 
         fee = (trade_value + profit) * rate
         self.available_capital -= fee
+        self.fees += fee
 
 
-    def _trade(self, ticker, trade_value, profit=0, side='long', method='open'):
+    def _trade(self, ticker, trade_value, profit=0, signal='long', method='buy'):
 
         base_value = trade_value / self.x_leverage
-        if side == 'buy':
+        if method == 'buy':
             self.available_capital -= base_value
-        else:  # side == 'sell'
+        else:  # method == 'sell'
             self.available_capital += base_value + profit
 
-        self._add_fee(ticker, trade_value, side, profit)
-
-
-    def _manage_positions(self, position, method):
-
-        if method == 'open':
-            self.positions.append(position)
-        elif method == 'close':
-            position_index = self.positions.index(position)
-            self.positions.pop(position_index)
+        self._add_fee(ticker, trade_value, profit, signal, method)
 
 
     def open(self, position):
 
         trade_value = self.available_capital * self.trade_size * self.x_leverage
         position['dollar_value'] = trade_value
-        self._trade(position['ticker'], trade_value, side=position['signal'], method='open')
-        self._manage_positions(position, method='open')
+        position['pct_open'] = 1
+
+        self._trade(position['ticker'], trade_value, signal=position['signal'], method='buy')
+        self.positions.append(position)
 
 
     def sell(self, position, index_of_action=None):
 
         tp_index = position['index_tp_hit'].index(index_of_action)
 
-        rr = self.rr_list[tp_index]
-        pct_sold = self.tp_pcts[tp_index]
+        pct_sold = self.tps[tp_index]['pct_sold']
+        rr = self.tps[tp_index]['rr']
 
         trade_value = position['dollar_value'] * pct_sold
         profit = rr * trade_value * position['pct_open'] * position['pct']
@@ -74,8 +72,16 @@ class Portfolio:
         position['index_tp_hit'][tp_index] = None
         position['pct_open'] -= pct_sold
 
-        self._trade(position['ticker'], trade_value, profit=profit, side='sell')
+        self._trade(position['ticker'], trade_value, profit=profit, signal=position['signal'], method='sell')
+
+
+    def close(self, position):
+
+        # Sell whatever remains of position
+        if position['pct_open'] > 0:
+            trade_value = position['dollar_value'] * position['pct_open']
+            self._trade(position['ticker'], trade_value, signal=position['signal'], method='sell')
 
         # Close position
-        if tp_index == position['tp']:
-            self._manage_positions(position, method='close')
+        pos_index = self.positions.index(position)
+        self.positions.pop(pos_index)
