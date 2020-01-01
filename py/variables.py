97b944760850a979e76a2683b2020af4e8273f69
Carter Carlson <carlfarterson@gmail.com>
2020-01-01 01:53:02 -0600

    Re-order variable declarations and modify tp_pcts_list to decimals

--- py/variables.py
+++ py/variables.py
@@ -1,9 +1,10 @@
 import ccxt
 from itertools import permutations
+import numpy as np
 
-
-# exchange = ccxt.bitfinex()
 exchange = ccxt.bitmex()
+
+tickers = ['BTC/USD', 'ETH/USD', 'ETHH20', 'LTCH20', 'XRPH20', 'BCHH20', 'EOSH20']
 
 
 # Telegram
@@ -17,21 +18,18 @@ atta_id = '@AllthingsTA'
 atta_insiders_id = '-1001456456400'
 
 
-tickers = ['BTC/USD', 'ETH/USD', 'ETHH20', 'LTCH20', 'XRPH20', 'BCHH20', 'EOSH20']
-
-
+# Moving average combinations
 avg1 = [8, 15, 21, 30]
 avg2 = [21, 25, 30, 34, 40, 55, 60]
 avg3 = [34, 40, 50, 55, 60, 75, 89, 100, 144]
 avgs_combined = [[a1, a2, a3] for a1 in avg1 for a2 in avg2 for a3 in avg3 if a1 < a2 and a2 < a3]
 
 
-pcts = list(range(0, 101, 10))
-pcts *= 4
-perms = permutations(pcts, 4)
+# Creating TP pct ranges
+pcts = list(np.arange(11)/10) * 4
+perms = set(permutations(pcts, 4))
 
-tp_pcts = set([x for x in perms if sum(x) == 100])
-tp_pcts_lst = []
+tp_pcts_list = [list(x) for x in perms if sum(x) == 1]
 
-for tp_pct in tp_pcts:
-    tp_pcts_lst.append(dict(zip(range(1,5), tp_pct)))
+for tp_pcts in tp_pcts_list:
+    tp_pcts.insert(0, 1)
