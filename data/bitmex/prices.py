6ccbc96f61e8e38ce6e6be43a1a127756ada8005
Carter Carlson <carlfarterson@gmail.com>
2020-02-16 14:38:48 -0700

    Save initial XRPUSD price data

--- data/bitmex/prices.py
+++ data/bitmex/prices.py
@@ -4,23 +4,27 @@ import numpy as np
 import ccxt
 from datetime import datetime, timedelta
 import time
-from py.variables import tickers
+
+signals = pd.read_csv('../../data/signals/atta_insiders.csv')
 
 bitmex = ccxt.bitmex()
-start = datetime(year=2019, month=6, day=14, hour=3)
+# tickers_traded_2020 = [x for x in signals['ticker'].unique() if '/H20' in x or '/USD' in x]
 
+start = datetime(year=2020, month=1, day=1, hour=0)
 
-for ticker in tickers:
+for ticker in ['XRP/USD']:
     start_date = start
     df = []
 
     while start_date < datetime.now():
         data = bitmex.fetch_ohlcv(ticker, '1h', limit=500, since=int(time.mktime(start_date.timetuple())*1000))
         df += data
-        start_date += timedelta(hours=len(data))
+
+        last_date = datetime.fromtimestamp(np.array(data)[-1, 0]/1000)
+        start_date = last_date + timedelta(hours=1)
         time.sleep(.5)
 
     df = pd.DataFrame(df, columns=['date', 'open', 'high', 'low', 'close', 'volume'])
     df['date'] = [datetime.fromtimestamp(x/1000) for x in df['date']]
 
-    df.to_csv('data/bitmex/' + ticker.replace('/','') + '.csv', index=False)
+    df.to_csv(f"{ticker.replace('/','')}.csv", index=False)
