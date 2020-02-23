986ff6c363cd06ffd81631827bd63a1c6978a70c
Carter Carlson <carlfarterson@gmail.com>
2020-02-23 00:40:07 -0600

    Add absolute file references

--- py/bot.py
+++ py/bot.py
@@ -1,25 +1,37 @@
-import pandas as pd
+from config import channels
+
 import logic
+
+import pandas as pd
+
 from datetime import datetime
 
+import os
 
-channels = {
-    'wc_elite': [15, 30, 50],
-    'atta_insiders': [21, 30, 55]
-}
+
+
+def determine_csv_filename(channel):
+    return f"{os.path.abspath('../')}/data/signals/{channel}.csv"
 
 
 def main(testing=False):
+
     for channel, averages in channels.items():
 
+        # Create dataframe of signals based on algorithm logic
         signal_df = logic.run(averages)
 
-        old_signal_df = pd.read_csv('/home/carter/peter-signal/data/signals/' + channel + '.csv')
+        # Load past saved signals
+        csv_filename = determine_csv_filename(channel)
+        old_signal_df = pd.read_csv(csv_filename)
+
+        # Compare our signals dataframe to the saved signals dataframe
+        # Only look at signals that are more recent than the saved signals
         new_signals = signal_df[signal_df['date'] > max(old_signal_df['date'])]
 
         if len(new_signals) > 0:
             old_signal_df = old_signal_df.append(new_signals, ignore_index=True, sort=False)
-            old_signal_df.to_csv('/home/carter/peter-signal/data/signals/' + channel + '.csv', index=False)
+            old_signal_df.to_csv(csv_filename, index=False)
 
             if not testing:
                 for _, row in new_signals.iterrows():
@@ -28,4 +40,5 @@ def main(testing=False):
 
 
 if __name__ == '__main__':
-    main()
+    main(testing=True)
+    # TODO: CLI with python arguments
