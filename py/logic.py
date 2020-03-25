b62c9cb42a8c332709d010dba053a1ec753c820e
Carter <carlfarterson@gmail.com>
2020-03-25 18:04:44 +0000

    Update signals to 3.25:

--- py/logic.py
+++ py/logic.py
@@ -100,10 +100,10 @@ def msg_wc(ticker, signal, stop_loss, low_price, high_price, tps):
     text += '🚨🚨🚨'
 
     # requests.get(url + urlencode({'chat_id': '@testgbot123', 'text': text}))
-    requests.get(url + urlencode({'chat_id': wc_elite_id, 'text': text}))
+    requests.get(url + urlencode({'chat_id': wc_id, 'text': text}))
 
-    if ticker in ['XRP/H20', 'LTC/H20', 'BCH/H20']:
-        requests.get(url + urlencode({'chat_id': wc_id, 'text': text}))
+    # if ticker in ['XRP/H20', 'LTC/H20', 'BCH/H20']:
+    requests.get(url + urlencode({'chat_id': wc_elite_id, 'text': text}))
 
 
 
@@ -119,7 +119,7 @@ def msg_atta(ticker, signal, stop_loss, low_price, high_price, tps):
     text += '💰💰@Allthingstaadmin💰💰'
 
     # requests.get(url + urlencode({'chat_id': '@testgbot123', 'text': text}))
-    requests.get(url + urlencode({'chat_id': atta_insiders_id, 'text': text}))
+    requests.get(url + urlencode({'chat_id': atta_id, 'text': text}))
 
-    if ticker in ['BTC/USD', 'ETH/USD',  'ETH/H20']:
-    	requests.get(url + urlencode({'chat_id': atta_id, 'text': text}))
+    # if ticker in ['BTC/USD', 'ETH/USD',  'ETH/H20']:
+    requests.get(url + urlencode({'chat_id': atta_insiders_id, 'text': text}))
